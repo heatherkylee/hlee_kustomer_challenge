2a0c4d2108bf68d0b51680f5db0a99f68629d73f
Heather Lee <heather.k.lee@gmail.com>
2018-11-09 22:10:28 -0500

    create work_phone and home_phone methods to determine phone type

--- data.rb
+++ data.rb
@@ -1,8 +1,31 @@
-class CustomerDataExport
+class CustomerDataImport
   
   # import data from csv file
   def initialize
     @customers = CSV.read('sample1.csv', headers: true).to_a
   end
 
+  # define type of phone number (work) through attribute name from data.csv
+  def work_phone
+    work_phone = @customers[0][5].to_s.split("")
+    i = 0
+    @work_phone_type = ""
+    while i < 4
+      @work_phone_type << work_phone[i]
+      i += 1
+    end
+    @work_phone_type
+  end
+  
+  # define tpye of phone number (home) through attribute name from data.csv
+  def home_phone
+    home_phone = @customers[0][4].to_s.split("")
+    i = 0
+    @home_phone_type = ""
+    while i < 4
+      @home_phone_type << home_phone[i]
+      i += 1
+    end
+    @home_phone_type
+  end
 end
