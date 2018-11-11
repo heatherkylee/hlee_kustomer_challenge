9d9df566e18da368c2593722b4e52a22981424f8
Heather Lee <heather.k.lee@gmail.com>
2018-11-11 12:39:34 -0500

    create hash for each customer that have values

--- data5.rb
+++ data5.rb
@@ -1,5 +1,3 @@
-# create new hash with key/value pairs based on condition
-
 require 'csv'
 require 'json'
 require 'uri'
@@ -7,131 +5,48 @@ require 'net/http'
 require 'openssl'
 require 'date'
 
-
-# @convert_to_json_hash = {
-#   name: "",
-#   emails: [
-#     {
-#       email: "" 
-#     }
-#   ],
-#   phones: [
-#   {
-#     type: "",
-#     phone: ""
-#   }],
-#   tags: [],
-#   birthdayAt: "" 
-# }
-
-
 CSV.foreach("sample1.csv", headers: true, header_converters: :symbol) do |row|  
   
   row.to_h
 
-  # puts row
-  # p "*" * 10
+  tag_list = []
 
+  @tap_hash = Hash.new.tap do |hash|
+    hash["name"] ="#{row[:firstname]} #{row[:lastname]}" if row[:firstname] && row[:lastname] 
+    # hash[:emails] = Hash.new([]).tap do |email|
+    hash["emails"] = Array.new().push(
+        Hash.new.tap do |email|
+          email["email"] = row[:email] if row[:email]
+        end
+      )
+
+    if row[:homephone] || row [:workphone] 
+      hash["phones"] = Array.new() 
+
+      home = Hash.new.tap do |phone1|
+        phone1["type"] = "home" if row[:homephone]
+        phone1["phone"] = row[:homephone] if row[:homephone]
+      end
+      hash["phones"] << home
  
-  email_list = []
-  phone_list = []
-  tap_hash = Hash.new.tap do |hash|
-    hash[:name] ="#{row[:firstname]} #{row[:lastname]}" if row[:firstname] && row[:lastname] 
-    hash[:emails] = Hash.new.tap do |email|
-      email[:email] = email_list << row[:email] if row[:email]
-    end
-    hash[:phones] = Hash.new.tap do |phone|
-      phone[:type] = phone_list << "home" if row[:homephone]
-      phone[:phone] = phone_list << row[:homephone] if row[:homephone]
-      phone[:type] = phone_list << "work" if row[:workphone]
-      phone[:phone] = phone_list << row[:workphone] if row[:workphone]
-    end
-#     {
-#       email: "" 
-#     }
-#   ],
-#   phones: [
-#   {
-#     type: "",
-#     phone: ""
-#   }],
-#   tags: [],
-#   birthdayAt: "" 
-# }
 
+      work = Hash.new.tap do |phone2|
+        phone2["type"] = "work" if row[:workphone]
+        phone2["phone"] = row[:workphone] if row[:workphone]
+      end
+      hash["phones"] << work
+    end
+
+    hash["tags"] = tag_list << row[:customertype] if row[:customertype]
+    hash["birthdayAt"] = DateTime.strptime(row[:birthday], "%a %b %d %Y %H:%M:%S %Z") if row[:birthday]
   end
 
-  p tap_hash
+  # p json_data = @tap_hash.to_json
+  puts JSON.pretty_generate(@tap_hash)
 
-# create full name
-#   if row[:firstname] == nil  && row[:lastname] == nil
-#     @fullname = nil
-#   else
-#     @fullname = "#{row[:firstname]} #{row[:lastname]}"
-#   end
-
-#   # parse birthday to date-time format
-#   if row[:birthday] == nil
-#     @birthday_at = nil
-#   else
-#     birthday = row[:birthday]
-#     format = "%a %b %d %Y %H:%M:%S %Z"
-#     @birthday_at = DateTime.strptime(birthday, format) 
-#   end
-
-#   # create home phone type
-#   if row[:homephone] == nil
-#     @phonetype_home = nil
-#     @phonenumber_home = nil
-#   else
-#     @phonenumber_home = row[:homephone]
-#     @phonetype_home = "home"
-#   end  
-
-#   # create work phone type
-#   if row[:workphone] == nil
-#     @phonetype_work = nil
-#     @phonenumber_work = nil
-#   else
-#     @phonetype_work = "work"
-#     @phonenumber_work = row[:workphone]
-#   end
-# # 
-#  #create customer tag
-#   if row[:customertype] == nil
-#     @tag = nil
-#   else
-#     @tag = row[:customertype]
-#   end
-
-#   @convert_to_json_hash =
-#     {
-#     name: @fullname,
-#     emails: [
-#       {
-#       email: row[:email]
-#       }
-#     ],
-#     phones:[
-#     {
-#       type: @phonetype_home,
-#       phone: @phonenumber_home
-#     },
-#     {
-#       type: @phonetype_work,
-#       phone: @phonenumber_work
-#     }
-#   ],
-#       tags: @tag,
-#     birthdayAt: @birthday_at
-  # }
-
-  # p json_data = @convert_to_json_hash
-  # puts JSON.pretty_generate(@convert_to_json_hash.compact)
-
-  # File.open("data4.json","a") do |f|
-  #   f.write(@convert_to_json_hash.to_json)
-  # end
+  File.open("data5.json","a") do |f|
+    f.write(@json_data)
+  end
   
   # url = URI("https://api.kustomerapp.com/v1/customers")
 
@@ -142,15 +57,8 @@ CSV.foreach("sample1.csv", headers: true, header_converters: :symbol) do |row|
   # request = Net::HTTP::Post.new(url)
   # request["authorization"] = 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjViZTNiNGJlYmZiYTU0MDA4ZTVjNGYxOSIsInVzZXIiOiI1YmUzYjRiZTNjODAyNjAwMTkzMGJhMDMiLCJvcmciOiI1YmUzNTJiNDNjODAyNjAwMTkyOGYzNjciLCJvcmdOYW1lIjoienp6LWhlYXRoZXIiLCJ1c2VyVHlwZSI6Im1hY2hpbmUiLCJyb2xlcyI6WyJvcmcuYWRtaW4iLCJvcmcudXNlciJdLCJleHAiOjE1NDIyNTQzOTcsImF1ZCI6InVybjpjb25zdW1lciIsImlzcyI6InVybjphcGkiLCJzdWIiOiI1YmUzYjRiZTNjODAyNjAwMTkzMGJhMDMifQ.jXv1zUdGtB_wbM8ySbitW4HHyfy4qytP4tqF5hM72nY'
   # request["content-type"] = 'application/json'
-  # request.body = (JSON.pretty_generate(@convert_to_json_hash))
+  # request.body = (@json_data)
 
   # response = http.request(request)
   # puts response.read_body
 end
-
-values = Hash.new.tap do |my_hash|
-  my_hash[:x] = 1 
-  my_hash[:y] = 2 
-end
-
-p values
